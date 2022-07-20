ea6e86d43c024ebf9a4e7bfc2b0e21f6ea1c6ba2
toremann <50668920+toremann@users.noreply.github.com>
2022-07-21 00:51:51 +0200

    full auth bcyptjs/jsonwebtoken

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -2,12 +2,13 @@ const asyncHandler = require('express-async-handler');
 const goalModel = require('../models/goalModel');
 
 const Goal = require('../models/goalModel');
+const User = require('../models/userModel');
 
 // @desc get goals
 // @route GET /api/goals
 // @access Private
 const getGoals = asyncHandler(async (req, res) => {
-    const goals = await Goal.find();
+    const goals = await Goal.find({ user: req.user.id });
 
     res.status(200).json(goals);
 });
@@ -23,6 +24,7 @@ const setGoals = asyncHandler(async (req, res) => {
 
     const goal = await Goal.create({
         text: req.body.text,
+        user: req.user.id,
     });
 
     res.status(200).json(goal);
@@ -39,6 +41,20 @@ const updateGoals = asyncHandler(async (req, res) => {
         throw new Error('Goal not found');
     }
 
+    const user = await User.findById(req.user.id);
+
+    // Check for user
+    if (!user) {
+        res.status(401);
+        throw new Error('User not found');
+    }
+
+    // Make sure the logged in user matches the goal user
+    if (goal.user.toString() !== user.id) {
+        res.status(401);
+        throw new Error('User not autherized');
+    }
+
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true });
 
     res.status(200).json(updatedGoal);
@@ -53,6 +69,20 @@ const deleteGoals = asyncHandler(async (req, res) => {
     if (!goal) {
         res.status(400);
         throw new Error('Goal not found');
+    }
+
+    const user = await User.findById(req.user.id);
+
+    // Check for user
+    if (!user) {
+        res.status(401);
+        throw new Error('User not found');
+    }
+
+    // Make sure the logged in user matches the goal user
+    if (goal.user.toString() !== user.id) {
+        res.status(401);
+        throw new Error('User not autherized');
     }
 
     await goal.remove();
